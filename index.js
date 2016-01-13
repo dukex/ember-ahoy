26ce80b96a7a70c9628e8a472c479fde68bcac03
Duke <dukekhaos@gmail.com>
2016-01-13 01:13:02 -0200

    setup ahoy config on head

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ module.exports = {
     this._super.included(app);
 
     app.import(app.bowerDirectory + '/ahoy/ahoy.js');
+  },
+  contentFor: function(target, config){
+    if(target === 'head') {
+      return `<script>window.ahoy = {};\nwindow.ahoy.visitsUrl = '${config.ahoy.visitsUrl}';\nwindow.ahoy.eventsUrl = '${config.ahoy.eventsUrl}';</script>`
+    }
   }
 };
